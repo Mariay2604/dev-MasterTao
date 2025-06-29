394878c4e2ff2a79a841c8c9c31e701af8c98ec2
Mariay2604 <mariay2604@ukr.net>
2025-06-29 18:47:58 +0300

    feat: added swiper

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -2,3 +2,21 @@ const btnBurger = document.querySelector('.hamburger')
 btnBurger.addEventListener('click', ()=> {
     btnBurger.classList.toggle('is-active')
 })
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    clickable: true,
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
