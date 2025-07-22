10fc16b73eddc2e40a8cf19af772dbbbb357a1d7
Mariay2604 <mariay2604@ukr.net>
2025-07-22 20:04:02 +0300

    refactor: global.scss

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,3 +1,6 @@
+const selects = document.querySelectorAll('select')
+selects.forEach(item => NiceSelect.bind(item))
+
 const btnBurger = document.querySelector('.hamburger')
 btnBurger.addEventListener('click', ()=> {
     btnBurger.classList.toggle('is-active')
@@ -33,3 +36,17 @@ const headerHeight = header.offsetHeight
 
 banner.setAttribute('style', '--header-height: ', + headerHeight + 'px')
 
+const InputCheckbox = document.querySelectorAll('input[type="checkbox"]')
+
+InputCheckbox.forEach(item => {
+  item.closest('.input-box').classList.add('input-box_checkbox')
+})
+
+const fileInput = document.querySelectorAll('[type="file"]');
+
+fileInput.forEach(item => {
+    item.addEventListener('change', function() {
+        let fileName = item.closest('.input-box').querySelector('.file-name')
+        fileName.innerHTML = item.files[0].name;
+    })
+})
